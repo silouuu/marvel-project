db8ad9d75d58467ace29646cf6fb9fa1933f1cd5
Siloe Rabadan <siloe.rabadan@etudiant.univ-lr.fr>
2020-10-15 13:55:02 +0200

    appel api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,7 @@
     </section>
     <section id="charaters" class="flex flex-col items-center">
       <h2>Characters</h2>
+      <p v-on:click="getUser()">aa</p>
       <CharacterFilters/>
       <CharacterList/>
     </section>
@@ -15,7 +16,10 @@
 import WelcomeSection from '@/components/WelcomeSection.vue';
 import CharacterFilters from '@/components/CharacterFilters.vue';
 import CharacterList from '@/components/CharacterList.vue';
-// import axios from "axios";
+
+const axios = require('axios');
+
+const md5 = require('md5');
 
 export default {
   name: 'Home',
@@ -24,7 +28,31 @@ export default {
     CharacterFilters,
     CharacterList,
   },
+  data() {
+    return {
+      dataAPI: Object,
+    };
+  },
+  beforeMount() {
+    this.getCharacters();
+  },
+  methods: {
+    async getCharacters() {
+      const publicKey = '123def4ba0e2cd014c5e7f4f671d018c';
+      const privateKey = 'a4916f029a0afef31c3be5c60b9814a2114afb42';
+      const apiUrl = 'https://gateway.marvel.com/v1/public/characters';
+      const ts = '1';
+      const hash = md5(ts + privateKey + publicKey);
+      console.log(hash);
+      const ApiGetUrl = `${apiUrl}?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+
+      const { data } = await axios.get(ApiGetUrl);
+      this.data = data;
+      console.log(this.data);
+    },
+  },
 };
+
 </script>
 <style  scoped lang="scss">
 #charaters{
